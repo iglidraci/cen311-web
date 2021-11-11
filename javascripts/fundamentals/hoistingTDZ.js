08d57697b15c9440d7d3ee696d35a53d831a78ec
iglidraci <iglidraci98@gmail.com>
2021-11-11 20:12:38 +0100

    javascript

---
+++ javascripts/fundamentals/hoistingTDZ.js
@@ -0,0 +1,92 @@
+
+// So var variables are hoisted to the top 
+// of their scope and initialized with a value of undefined
+
+console.log(`first name is ${firstName}`);
+// console.log(age);
+
+// this is a problem that you might occure with var
+var position = 'junior';
+
+var workingYears = 20;
+
+if (workingYears > 5) {
+    var position = 'senior';
+}
+
+console.log(`position is ${position}`);
+
+
+// let is block scoped
+// a variable declared in a block with let 
+// is only available within that block
+
+// let's resolve the problem above
+
+let greeting = 'good morning';
+let hour = 16;
+if (hour > 12){
+    let greeting = 'good afternoon';
+}
+console.log(greeting);
+
+// like var, let can be updated within its scope
+// unlike var, let can't be redeclared in the same scope
+// hoisting of let
+// if you try to use a let variable before declaration => Reference Error
+
+
+const person1 = {
+    name: 'John',
+    age: 30,
+    sayHello: function() {
+        console.log(`${this.name}, ${this.age} years old says hello`);
+    }
+}
+
+// const cannot be updated or re-declared
+
+person1.sayHello();
+
+person1.name = 'Jim';
+person1.sayHello();
+
+// this below will give an error Assignment to constant variable.
+// person1 = 'hello';
+
+// differences
+
+/**
+ * They are all hoisted to the top of their scope.
+ * var variables are initialized with undefined
+ * let and const variables are not initialized
+ */
+
+
+
+
+var firstName = "john";
+let age = 23;
+const job = "dev";
+
+console.log(addDec(1, 2));
+// console.log(addExp(1, 2));
+
+function addDec(a, b) {
+    return a + b;
+}
+
+const addExp = function(a, b) {
+    return a + b;
+}
+
+// because of hoisting, it will be initialized as undefined and we try to call it
+// var addExp = function(a, b) {
+//     return a + b;
+// }
+
+const addLambda = (a, b) => a+b;
+
+var x = 1;
+let y = 2;
+const z = 3;
