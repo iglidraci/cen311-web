6ef9a8d4ddfbb25f20fc73fe32c5c6deb8370937
igli <iglidraci98@gmail.com>
2022-12-06 01:47:21 +0100

    first image

--- 2022-2023/week7_javascript/concurrency/scripts/05_first_image.js
+++ 2022-2023/week7_javascript/concurrency/scripts/05_first_image.js
@@ -4,18 +4,27 @@
 
 async function fetchAndDecode(url, description) {
     const res = await fetch(url);
+    console.log(res);
     if (!res.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        console.log(res);
+        throw new Error(`HTTP error! status: ${res.status}`);
     }
-    const data = await response.blob();
+    const data = await res.blob(); //  file-like object of immutable, raw data
     return [data, description];
 }
 
-const coffee = fetchAndDecode("images/capybara.jpeg", "Coffee");
-const tea = fetchAndDecode("images/caracal.jpeg", "Tea");
+const capybara = fetchAndDecode("images/capybara.jpeg", "capybara");
+const caracal = fetchAndDecode("images/caracal.jpeg", "caracal");
 
-Promise.any([coffee, tea])
-.then(([blob, description]) => {
+Promise.any([capybara, caracal])
+.then((data) => {
+    const [blob, description] = data;
+    console.log(blob); // size in bytes
+    // A string indicating the MIME type of the data contained in the Blob
+    console.log(blob.arrayBuffer()); // promise
+    console.log(blob.text()); // promise
+    // static method creates a string containing a URL representing 
+    // the object given in the parameter.
     const objectURL = URL.createObjectURL(blob);
     const image = document.createElement("img");
     image.src = objectURL;
@@ -25,4 +34,3 @@ Promise.any([coffee, tea])
 .catch((e) => {
     console.log(e);
 });
-  
